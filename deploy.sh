283862f82e5aa6eaf9d578a1d80f5861964c7e33
Florian Barth <florianbarth@gmx.de>
2016-04-19 16:36:56 +0200

    replaced copy script by symlink script
    Now takes a lot less manual management from me :)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-rm *~
-cp .* ~
+
+ln -s ~/workspaces/bash/bash_config/.emacs ~/.emacs
+ln -s ~/workspaces/bash/bash_config/.bashrc ~/.bashrc
+ln -s ~/workspaces/bash/bash_config/.bash_login ~/.bash_login
+ln -s ~/workspaces/bash/bash_config/.bash_logout ~/.bash_logout
+ln -s ~/workspaces/bash/bash_config/.bash_profile ~/.bash_profile
+ln -s ~/workspaces/bash/bash_config/.bashrc ~/.bashrc
