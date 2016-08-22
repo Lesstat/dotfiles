fcdbdcd4cc9c40d9f534b977376d82324803a726
Florian Barth <florianbarth@gmx.de>
2016-08-22 10:25:18 +0200

    Create template folder for org-capture
    The template folder also get symlinked into .emacs.d for simple configuration.
    First template is for gtd-style weekly review.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 current_path=$(pwd)
 ln -sf ${current_path}/.emacs ~/.emacs
 ln -sf ${current_path}/config.org ~/.emacs.d/config.org
+ln -sf ${current_path}/org-capture-template ~/.emacs.d/org-capture-template
 touch ~/.emacs.d/custom.el #File needs to be existing for emacs config to work
 ln -sf ${current_path}/.bashrc ~/.bashrc
 ln -sf ${current_path}/.bash_login ~/.bash_login
