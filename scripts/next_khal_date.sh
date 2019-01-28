85e1a2eb2df1f90c11cdcc81aaa7db1eaa67c679
Florian Barth <florianbarth@gmx.de>
2019-01-28 07:35:44 +0100

    [polybar] Hide calendar widget if no events are scheduled today

--- scripts/next_khal_date.sh
+++ scripts/next_khal_date.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 now=$(date +%H:%M)
-khal list today "${now}" today 23:59 | sed -e "/^[^ ]*day,/d" -e "/↦↔⇥/d" -e "2q" 
+khal list today "${now}" today 23:59 | sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" 
+echo ""
