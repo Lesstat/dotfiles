57339843d5102dd1412a3a49d659beb3a3b183f2
Florian Barth <florianbarth@gmx.de>
2019-02-14 06:36:36 +0100

    [Polybar] limit length of calendar widget to 30 chars

--- polybar/bin/next_khal_date.sh
+++ polybar/bin/next_khal_date.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 now=$(date +%H:%M)
-khal list today "${now}" today 23:59 | sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" 
+khal list today "${now}" today 23:59 | sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" | cut -c 1-30
 echo ""
