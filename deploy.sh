38fe69f5ea79190752e94ac9dade2b7a83ade309
Florian Barth <florianbarth@gmx.de>
2019-07-25 08:03:39 +0200

    [deploy.sh] Improve deploying scimax and mpd

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,10 @@ if ! which fd &> /dev/null; then
     echo "   sudo pacman -S fd"
     exit 1
 fi 
+
+git submodule update --init --recursive emacs/.emacs.d/scimax # don't forget to initialize scimax
 touch ~/.emacs.d/custom.el #File needs to be existing for emacs config to work
+if [ ! -d ~/.config/mpd ]; then
+    mkdir -p ~/.config/mpd # less clutter if the directory exists beforehand
+fi
 fd -t d -d 1 -x stow -t ~ {} 
