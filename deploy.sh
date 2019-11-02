b70b722fcf6c0f4f0142ff9744f0849d430ac8da
Florian Barth <florianbarth@gmx.de>
2019-11-02 14:43:00 +0100

    [deploy.sh] Update all submodules

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ if ! which fd &> /dev/null; then
     exit 1
 fi 
 
-git submodule update --init --recursive emacs/.emacs.d/scimax # don't forget to initialize scimax
+git submodule update --init --recursive # don't forget to initialize the submodules
 touch ~/.emacs.d/custom.el #File needs to be existing for emacs config to work
 if [ ! -d ~/.config/mpd ]; then
     mkdir -p ~/.config/mpd # less clutter if the directory exists beforehand
