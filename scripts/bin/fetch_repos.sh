827e18a8248031cb6a4d599ab6f26487a0fdab2c
Florian Barth <barth@fmi.uni-stuttgart.de>
2020-02-13 17:13:49 +0100

    [fetch_repos.sh] Only look for git repos with defined remotes

--- scripts/bin/fetch_repos.sh
+++ scripts/bin/fetch_repos.sh
@@ -3,19 +3,21 @@
 PARENT_DIR=$1
 
 function fetchpull() {
-    if ! git -C "$1" fetch --all &> /dev/null ; then
-	echo "could not fetch ${1}"
+    DIR=$(cd "$1/../.."; echo ${PWD})
+    if ! git -C "${DIR}" fetch --all &> /dev/null ; then
+	echo "could not fetch ${DIR}"
+	exit 1
     fi
     
-    if ! git -C "$1" pull --ff-only &> /dev/null; then
-	echo "could not pull ${1}"
+    if ! git -C "${DIR}" pull --ff-only &> /dev/null; then
+	echo "could not pull ${DIR}"
     fi
 }
 
 export -f  fetchpull
 
 if which fd &> /dev/null ; then
-    fd -HI -t d "^\.git$" "${PARENT_DIR}" -x  bash -c "fetchpull {//}"
+    fd -HI -t d --full-path "\.git/refs/remotes$" "${PARENT_DIR}" -x  bash -c "fetchpull {//}"
 else				
-    find "${PARENT_DIR}" -type d -name .git -exec bash -c "fetchpull {}" \;
+    find "${PARENT_DIR}" -type d -path "*.git/refs/remotes" -exec bash -c "fetchpull {}" \;
 fi
