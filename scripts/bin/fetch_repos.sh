cb474a391442d314fb0eaf8e619b15df3ba605a3
Florian Barth <florianbarth@gmx.de>
2020-06-30 09:28:31 +0200

    [fetch_repos] Don't pull doom repo automatically

--- scripts/bin/fetch_repos.sh
+++ scripts/bin/fetch_repos.sh
@@ -3,21 +3,24 @@
 PARENT_DIR=$1
 
 function fetchpull() {
-    DIR=$(cd "$1/../.."; echo ${PWD})
-    if ! git -C "${DIR}" fetch --all &> /dev/null ; then
-	echo "could not fetch ${DIR}"
-	exit 1
-    fi
-    
-    if ! git -C "${DIR}" pull --ff-only &> /dev/null; then
-	echo "could not pull ${DIR}"
-    fi
+	DIR=$(
+		cd "$1/../.." || exit
+		echo "${PWD}"
+	)
+	if ! git -C "${DIR}" fetch --all &>/dev/null; then
+		echo "could not fetch ${DIR}"
+		exit 1
+	fi
+
+	if ! git -C "${DIR}" pull --ff-only &>/dev/null; then
+		echo "could not pull ${DIR}"
+	fi
 }
 
-export -f  fetchpull
+export -f fetchpull
 
-if which fd &> /dev/null ; then
-    fd -HI -t d --full-path "\.git/refs/remotes$" "${PARENT_DIR}" -x  bash -c "fetchpull {//}"
-else				
-    find "${PARENT_DIR}" -type d -path "*.git/refs/remotes" -exec bash -c "fetchpull {}" \;
+if which fd &>/dev/null; then
+	fd -HI -t d --full-path "\.git/refs/remotes$" -E emacs "${PARENT_DIR}" -x bash -c "fetchpull {//}"
+else
+	find "${PARENT_DIR}" -type d -path "*.git/refs/remotes" -exec bash -c "fetchpull {}" \;
 fi
