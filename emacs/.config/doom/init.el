b3eb9259480ba59b60895c091f771597cc12d158
Florian Barth <florianbarth@gmx.de>
2020-06-30 09:26:31 +0200

    [Emacs] add python-lsp yml & json (+ update doom)

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -151,7 +151,8 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python
+        +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
@@ -165,6 +166,8 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
+       yaml
+       json
 
        :email
        ;; (mu4e +gmail)
