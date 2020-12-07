fe889166ce4e58dc4457168b36c77e0d4d1e8928
Florian Barth <florianbarth@gmx.de>
2020-12-07 11:41:53 +0100

    [polybar] update comment in mail widget

--- polybar/bin/muWidget.sh
+++ polybar/bin/muWidget.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-hash mu > /dev/null 2>&1 || exit # leave if notmuch is not present
+hash mu >/dev/null 2>&1 || exit # leave if mu is not present
 
 mails=$(mu find maildir:"/.*Inbox/" 2> /dev/null | wc -l)
 
