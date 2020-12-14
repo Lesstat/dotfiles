a6475e381aac6731857b9de00bd099f3b3e86849
Florian Barth <florianbarth@gmx.de>
2020-12-14 13:25:35 +0100

    [polybar] Also include unread mails into mu widget

--- polybar/bin/muWidget.sh
+++ polybar/bin/muWidget.sh
@@ -2,10 +2,10 @@
 
 hash mu >/dev/null 2>&1 || exit # leave if mu is not present
 
-mails=$(mu find maildir:"/.*Inbox/" 2> /dev/null | wc -l)
+mails=$(mu find flag:unread OR maildir:"/.*Inbox/" 2>/dev/null | wc -l)
 
 if [ "$mails" -gt 0 ]; then
-    echo "" 
+	echo ""
 else
-    echo ""
-fi	
+	echo ""
+fi
