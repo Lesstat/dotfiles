0131de71d02e2265612276cad87e5e3978d0e30c
Florian Barth <florianbarth@gmx.de>
2020-12-14 17:04:10 +0100

    [polybar] read ignored calendars from config file

--- polybar/bin/next_khal_date.sh
+++ polybar/bin/next_khal_date.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+exec 2>/dev/null
 
 now=$(date +%H:%M)
-khal list today "${now}" today 23:59 | sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" | cut -c 1-30
+
+command="khal list"
+while IFS= read -r line 2>/dev/null; do
+	command="${command} -d \"${line}\""
+done <~/.config/calwidget/ignored_calendars
+
+eval "${command}" today "${now}" today 23:59 |
+	sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" |
+	cut -c 1-30
 echo ""
