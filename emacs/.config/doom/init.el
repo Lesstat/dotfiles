f267be28d8cfa316463de1ea997bf70dbbb3fa94
Florian Barth <florianbarth@gmx.de>
2021-03-11 09:29:05 +0100

    Activate undo module in doom

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -66,6 +66,7 @@
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
+       undo
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
