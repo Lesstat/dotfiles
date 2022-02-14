e10db389505e72623baf5c37758f087c821d47cc
Florian Barth <florianbarth@gmx.de>
2022-02-14 11:29:03 +0100

    Make khal widget handle whole day events better

--- polybar/bin/next_khal_date.sh
+++ polybar/bin/next_khal_date.sh
@@ -8,8 +8,19 @@ command="khal list"
 while IFS= read -r line 2>/dev/null; do
 	command="${command} -d \"${line}\""
 done <~/.config/calwidget/ignored_calendars
+dates=$(eval "${command}" today "${now}" today 23:59)
 
-eval "${command}" today "${now}" today 23:59 |
-	sed -e "/^[^ ]*day,/d" -e "/[↦↔⇥]/d" -e "/No events/d" -e "2q" |
+wholeday=$(
+	echo "$dates" | sed -e "/^[^ ]*day,/d" \
+		-e "s/^[^0-9].*/📆/" \
+		-e "q"
+)
+printf "${wholeday} "
+echo "${dates}" |
+	sed -e "/^[^ ]*day,/d" \
+		-e "/^[^0-9]/d" \
+		-e "/[↦↔⇥]/d" \
+		-e "/No events/d" \
+		-e "q" |
 	cut -c 1-30
 echo ""
