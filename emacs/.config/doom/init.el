0254da88667319e236bcce419b7e75959e1ae720
Florian Ihle <florian@ilorian.xyz>
2024-10-13 13:54:04 +0200

    activate js support in doom

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -128,7 +128,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex
